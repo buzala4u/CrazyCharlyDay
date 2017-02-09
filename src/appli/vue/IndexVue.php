1ed8158413d9143f3f5426c019771c784667ef4b
Felix Durand <felix.durand4@etu.univ-lorraine.fr>
2017-02-09 15:25:30 +0100

    Ajout dun template

--- src/appli/vue/IndexVue.php
+++ src/appli/vue/IndexVue.php
@@ -500,14 +500,14 @@ class IndexVue {
 
 	
 
-	<script type="text/javascript" src="files/js/jquery-1.11.1.min.js"></script>
-	<script type="text/javascript" src="files/js/bootstrap.min.js"></script>
+	<script type="text/javascript" src="src/appli/vue/files/js/jquery-1.11.1.min.js"></script>
+	<script type="text/javascript" src="src/appli/vue/files/js/bootstrap.min.js"></script>
 	<!-- SLIDER REVOLUTION 4.x SCRIPTS  -->
-    <script src="files/rs-plugin/js/jquery.themepunch.tools.min.js"></script>
-    <script src="files/rs-plugin/js/jquery.themepunch.revolution.min.js"></script>
+    <script src="src/appli/vue/files/rs-plugin/js/jquery.themepunch.tools.min.js"></script>
+    <script src="src/appli/vue/files/rs-plugin/js/jquery.themepunch.revolution.min.js"></script>
 
-	<script type="text/javascript" src="files/js/plugins.js"></script>
-	<script type="text/javascript" src="files/js/custom.js"></script>
+	<script type="text/javascript" src="src/appli/vue/files/js/plugins.js"></script>
+	<script type="text/javascript" src="src/appli/vue/files/js/custom.js"></script>
 
 </body>
 </html>';
